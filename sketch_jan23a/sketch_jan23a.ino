8b7099fb742e4ea8f070e34544bd0a32f8a45cb7
Crmayb2 <crmayb2@ilstu.edu>
2025-01-23 16:29:43 -0600

    LED always on at less than 100

--- sketch_jan23a/sketch_jan23a.ino
+++ sketch_jan23a/sketch_jan23a.ino
@@ -1,20 +1,12 @@
 void setup() {
   pinMode(A6, INPUT);
+  pinMode(4, OUTPUT);
   int lightLevel = analogRead(A6);
   Serial.begin(9600);
   Serial.println(lightLevel);
-  if(lightLevel < 100) {
-    Serial.println("It's really dark");
+  if(lightLevel < 100){
+    digitalWrite (4, HIGH);
   }
-  if(lightLevel < 200) {
-    Serial.println("It's dim in here");
-  }
-  if(lightLevel < 700) {
-    Serial.println("It's a little bright");
-  }
-  if(lightLevel < 1024) {
-    Serial.println("It's really bright!");
-  } 
 
   // put your setup code here, to run once:
 
